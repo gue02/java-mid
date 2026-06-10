8b26567a6e646bb1273a7a41a51abb98b7aa4016
gue02 <kyu6716@naver.com>
2026-06-10 22:59:14 +0900

    java-mid 3-1(String Class)

---
+++ src/lang/string/method/StringUtilMain1.java
@@ -0,0 +1,9 @@
+package lang.string.method;
+
+public class StringUtilMain1 {
+    public static void main(String[] args) {
+        int num = 100;
+        boolean bool = true;
+        Object obj = new Object();
+    }
+}
