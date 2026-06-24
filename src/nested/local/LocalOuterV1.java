53e838f881782859f29e1eed736411e1986138bb
gue02 <kyu6716@naver.com>
2026-06-25 00:00:17 +0900

    Java_mid1(8.Nested/Inner Class 2)

---
+++ src/nested/local/LocalOuterV1.java
@@ -0,0 +1,26 @@
+package nested.local;
+
+public class LocalOuterV1 {
+    private int outInstanceVar = 3;
+
+    public void process(int ParamVar){
+        int localVar = 1;
+
+        class LocalPrinter{
+            int value = 0;
+
+            public void printData(){
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("ParamVar = " + ParamVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+        LocalPrinter printer = new LocalPrinter();
+        printer.printData();
+    }
+    public static void main(String[] args) {
+        LocalOuterV1 localOuterV1 = new LocalOuterV1();
+        localOuterV1.process(2);
+    }
+}
